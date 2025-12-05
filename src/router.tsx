9b15fda1da5e5b47ed07886914608c0ec83a4f7d
calie-png <calie@pivotandprospermarketing.com>
2025-12-05 18:35:48 -0500

    Update router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -2,8 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import AppLayout from "./layout/AppLayout";
 
 import Home from "./pages/Home";
-import Dashboard from "./pages/Dashboard";
 import Clients from "./pages/Clients";
+import Dashboard from "./pages/Dashboard";
 import CheckIn from "./pages/CheckIn";
 import Intake from "./pages/Intake";
 
