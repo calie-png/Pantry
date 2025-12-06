760e9f91c16ddf946497c2b3c3d02bce43b8d714
calie-png <calie@pivotandprospermarketing.com>
2025-12-06 08:22:06 -0500

    Update router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -4,6 +4,7 @@ import AppLayout from "./layout/AppLayout";
 import Home from "./pages/Home.tsx";
 import Dashboard from "./pages/Dashboard.tsx";
 import ClientsPage from "./pages/ClientsPage.tsx";
+import ClientDetailsPage from "./pages/ClientDetailsPage.tsx";
 import CheckInPage from "./pages/CheckInPage.tsx";
 import IntakePage from "./pages/IntakePage.tsx";
 import NewClientPage from "./pages/NewClientPage.tsx";
@@ -16,6 +17,7 @@ const router = createBrowserRouter([
       { index: true, element: <Home /> },
       { path: "dashboard", element: <Dashboard /> },
       { path: "clients", element: <ClientsPage /> },
+      { path: "clients/:id", element: <ClientDetailsPage /> },
       { path: "clients/new", element: <NewClientPage /> },
       { path: "checkin", element: <CheckInPage /> },
       { path: "intake", element: <IntakePage /> },
