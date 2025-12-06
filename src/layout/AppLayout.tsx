5ef925d20a83cffed4cf9e30b424626b8f749256
calie-png <calie@pivotandprospermarketing.com>
2025-12-05 22:11:07 -0500

    Update AppLayout.tsx

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -18,7 +18,7 @@ export default function AppLayout() {
               `block px-4 py-2 rounded-md ${
                 isActive
                   ? "bg-slateblue text-white"
-                  : "text-slateblue hover:text-white hover:bg-slateblue/70"
+                  : "text-gray-800 hover:text-white hover:bg-slateblue/70"
               }`
             }
           >
@@ -31,7 +31,7 @@ export default function AppLayout() {
               `block px-4 py-2 rounded-md ${
                 isActive
                   ? "bg-slateblue text-white"
-                  : "text-slateblue hover:text-white hover:bg-slateblue/70"
+                  : "text-gray-800 hover:text-white hover:bg-slateblue/70"
               }`
             }
           >
@@ -44,7 +44,7 @@ export default function AppLayout() {
               `block px-4 py-2 rounded-md ${
                 isActive
                   ? "bg-slateblue text-white"
-                  : "text-slateblue hover:text-white hover:bg-slateblue/70"
+                  : "text-gray-800 hover:text-white hover:bg-slateblue/70"
               }`
             }
           >
@@ -57,7 +57,7 @@ export default function AppLayout() {
               `block px-4 py-2 rounded-md ${
                 isActive
                   ? "bg-slateblue text-white"
-                  : "text-slateblue hover:text-white hover:bg-slateblue/70"
+                  : "text-gray-800 hover:text-white hover:bg-slateblue/70"
               }`
             }
           >
